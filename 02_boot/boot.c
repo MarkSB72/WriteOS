35de87899cf8fbf9a597a2f3d0c759b233d809d6
mark72 <132270789@qq.com>
2025-11-23 17:42:35 +0000

    1. add mmu init and test function

--- 02_boot/boot.c
+++ 02_boot/boot.c
@@ -1,6 +1,8 @@
+#include "mmu.h"
+
 typedef void (*init_func)(void);
 
-#define UFCON0 ((volatile unsigned int *)(0x101f1000)) 
+#define UFCON0 ((volatile unsigned int *)(0x101F1000)) 
 
 void hello_world(void)
 {
@@ -24,6 +26,9 @@ void plat_boot(void)
     {
         init[i]();
     }
+    init_sys_mmu();
+    start_mmu();
+    test_mmu();
     while(1);
 }
 
